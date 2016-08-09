329bf1d07f3a74a05886a2f35658fcd464fb9e8b
jeffreydorney <jjdorney1@gmail.com>
2016-08-09 15:35:47 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tiy-homework-clojure-madlib
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tiy_homework_clojure_madlib/core_test.clj
@@ -0,0 +1,7 @@
+(ns tiy-homework-clojure-madlib.core-test
+  (:require [clojure.test :refer :all]
+            [tiy-homework-clojure-madlib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
